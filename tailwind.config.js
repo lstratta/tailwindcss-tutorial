843a443add44639fb5689255f25a11d9969ddc22
pop-linux <pop-linux@pop-os.localdomain>
2022-11-23 11:54:25 +0000

    finished tutorial

--- tailwind.config.js
+++ tailwind.config.js
@@ -14,10 +14,10 @@ module.exports = {
         brightRedLight: 'hsl(12, 88%, 69%)',
         brightRedSupLight: 'hsl(12, 88%, 95%)',
         darkBlue: 'hsl(228, 39%, 23%)',
-        darkGrayishBlue: 'hsl(227, 12%, 61%)',
+        darkGreyishBlue: 'hsl(227, 12%, 61%)',
         veryDarkBlue: 'hsl(233, 12%, 13%)',
         veryPaleRed: 'hsl(13, 100%, 96%)',
-        veryLightGray: 'hsl(0, 0%, 98%)',
+        veryLightGrey: 'hsl(0, 0%, 98%)',
       }
     },
   },
